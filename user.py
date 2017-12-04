4491692be0a02ea440ae8ceb1ea2dac1ede94f79
rolingz <ning_ML@126.com>
2017-12-04 23:22:04 +0800

    test git

---
+++ user.py
@@ -0,0 +1,29 @@
+# class User():
+#     """定义一个用户简介"""
+#
+#     def __init__(self,first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.login_attempts = 0
+#
+#     def describe_user(self):
+#         print("the name is " + self.first_name + " " + self.last_name)
+#
+#     def greet_user(self):
+#         name = self.first_name + " " + self.last_name
+#         print("welcome back: " + name + " !")
+#
+#     def increment_login_attempts(self):
+#         self.login_attempts += 1
+#
+#     def reset_login_attempts(self):
+#         self.login_attempts = 0
+#
+#
+# class Nu():
+#
+#     def __init__(self, number):
+#         self.number = number
+#
+#     def imput(self):
+#         print(self.number)
