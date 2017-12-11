958bf58e22a74295031d76bf8bf9016b761f4aa2
rolingz <ning_ML@126.com>
2017-12-11 17:48:20 +0800

    fix

---
+++ python study/admin.py
@@ -0,0 +1,19 @@
+# from user import User
+#
+#
+# class Admin(User):
+#     """管理员的独特之处"""
+#
+#     def __init__(self, first_name, last_name):
+#         """初始化父类属性"""
+#         super().__init__(first_name, last_name)
+#         self.privileges = ["can add post", "can delete post", "can ban user"]
+#
+#     def show_privileges(self, number):
+#         name = self.first_name + self.last_name
+#         print(name + "`s privileges is that " + self.privileges[number])
+#
+#
+#
+#
+#
