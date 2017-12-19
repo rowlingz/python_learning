2eaf542baf1b5a2b3027ef4923ddc0a32923291b
rolingz <ning_ML@126.com>
2017-12-19 21:20:25 +0800

    合并两个链表 --

--- jianzhi_offer/ReverseList.py
+++ jianzhi_offer/ReverseList.py
@@ -7,31 +7,51 @@ class ListNode:
 class Solution:
     # 返回合并后列表
     def Merge(self, pHead1, pHead2):
-        if pHead1 is None:
-            return pHead2
-        if pHead2 is None:
-            return pHead1
-        p1 = pHead1
-        p2 = pHead2
         newHead = ListNode(None)
         newp = newHead
-        while p1 is not None and p2 is not None:
-            if p1.val < p2.val:
-                newp.next = ListNode(p1.val)
+        while pHead1 and pHead2:
+            if pHead1.val <= pHead2.val:
+                newp.next = ListNode(pHead1.val)
                 newp = newp.next
-                p1 = p1.next
-                continue
-            if p2.val < p1.val:
-                newp.next = ListNode(p2.val)
+                pHead1 = pHead1.next
+            else:
+                newp.next = ListNode(pHead2.val)
                 newp = newp.next
-                p2 = p2.next
-                continue
-        if p1 is not None:
-            newp.next = p1
-        if p2 is not None:
-            newp.next = p2
+                pHead2 = pHead2.next
+        if pHead1:
+            newp.next = pHead1
+        elif pHead2:
+            newp.next = pHead2
         newHead = newHead.next
         return newHead
+
+
+        # if pHead1 is None:
+        #     return pHead2
+        # if pHead2 is None:
+        #     return pHead1
+        # p1 = pHead1
+        # p2 = pHead2
+        # newHead = ListNode(None)
+        # newp = newHead
+        # while p1 is not None and p2 is not None:
+        #     if p1.val < p2.val:
+        #         newp.next = ListNode(p1.val)
+        #         newp = newp.next
+        #         p1 = p1.next
+        #         continue
+        #     if p2.val < p1.val:
+        #         newp.next = ListNode(p2.val)
+        #         newp = newp.next
+        #         p2 = p2.next
+        #         continue
+        # if p1 is not None:
+        #     newp.next = p1
+        # if p2 is not None:
+        #     newp.next = p2
+        # newHead = newHead.next
+        # return newHead
+
 
 
 
