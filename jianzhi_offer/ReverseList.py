e93564087e5344d560063b51d31233d740c4a577
rolingz <ning_ML@126.com>
2017-12-19 20:18:32 +0800

    合并两个链表 --

--- jianzhi_offer/ReverseList.py
+++ jianzhi_offer/ReverseList.py
@@ -7,21 +7,32 @@ class ListNode:
 class Solution:
     # 返回合并后列表
     def Merge(self, pHead1, pHead2):
-
-
-        while pHead2.next is not None:
-            p1 = pHead1
-            p2 = pHead2
-            if p2.val <= p1.val:
-                new = ListNode(p2.val)
-                new.next = p1
-                p1.next = new
-                pHead2 = pHead2.next
+        if pHead1 is None:
+            return pHead2
+        if pHead2 is None:
+            return pHead1
+        p1 = pHead1
+        p2 = pHead2
+        newHead = ListNode(None)
+        newp = newHead
+        while p1 is not None and p2 is not None:
+            if p1.val < p2.val:
+                newp.next = ListNode(p1.val)
+                newp = newp.next
+                p1 = p1.next
                 continue
-            if p2.val > p1.val:
-                pHead1 = pHead1.next
+            if p2.val < p1.val:
+                newp.next = ListNode(p2.val)
+                newp = newp.next
+                p2 = p2.next
                 continue
-        return pHead1
+        if p1 is not None:
+            newp.next = p1
+        if p2 is not None:
+            newp.next = p2
+        newHead = newHead.next
+        return newHead
+
 
 
 p1 = ListNode(1)
