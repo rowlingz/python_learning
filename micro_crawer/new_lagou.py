8b15d808301195deb68f9f709070afab04e0181b
micro <peichuan.mpc@alibaba-inc.com>
2017-12-26 22:11:32 +0800

    fix crawer

--- micro_crawer/new_lagou.py
+++ micro_crawer/new_lagou.py
@@ -3,12 +3,14 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 import json
+import time,random
 # print(sys.modules.keys())
 # for k in sys.modules.keys() :
 #     if k == 'requests' :
 #         print('存在requests模块')
 #         exit(0)
 # print('不存在')
+
 
 result = requests.get('http://www.yinyuetai.com/')
 ## test requests lib
@@ -32,12 +34,13 @@ headers = {
     'Upgrade-Insecure-Requests':'1',
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
 }
-page_index = 1
+page_index = 4
 while True :
     query_params['page'] = page_index
     print('开始爬取 ---- > ' + str(page_index) + '页')
     result = requests.get(url = 'http://www.zhipin.com/job_detail/?query=%E7%AE%97%E6%B3%95&scity=101210100&source=2' , params = query_params, headers = headers)
     if result.status_code == 200 :
+        
         # print(result.text.encode(result.encoding).decode('utf-8'))
         html_body = result.text.encode(result.encoding).decode('utf-8')
         ## 打印整个文本
@@ -61,6 +64,9 @@ while True :
         desp_map = {}
         for content_url in content_url_list :
             next_url = url + content_url
+            print('每个具体职位请求休眠一下')
+            time.sleep(random.randint(3,5))
+            print('启动...')
             result = requests.get(url = next_url, params=None, headers = headers)
             if result.status_code == 200 :
                 html_body = result.text.encode(result.encoding).decode('utf-8')
@@ -71,9 +77,14 @@ while True :
                         print(desc.text)
                         print('--------- 分割线 --------')
                         desp_map[url] = desc.text
-
+            else :
+                print('http status -> ' + str(result.status_code) + ', reason -> ' + result.reason)
+                break
         page_index += 1
+    elif result.status_code == 403 :
+        print('服务被拒绝了')
     else :
-        print('response status error')
+        print('http status = ' + str(result.status_code) + ", reason = " + result.reason)
+        break
 
 ## IP被限制，考虑代理
