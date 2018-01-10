aff366b52a383006be402c6a117f1a2a12649eff
rolingz <ning_ML@126.com>
2018-01-10 20:54:36 +0800

    借助 生成器进行全排列

--- jianzhi_offer/Permutation.py
+++ jianzhi_offer/Permutation.py
@@ -2,17 +2,49 @@
 # 例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba
 
 
+# class Solution:
+#     def Permutation(self, ss):
+#         li = list(ss)
+#         if len(li) < 1:
+#             yield ss
+#         else:
+#             for i in range(len(li)):
+#                 li[0], li[i] = li[i], li[0]
+#                 for j in self.Permutation(''.join(li[1:])):
+#                     yield str(li[0]) + str(j)
+#
+# s = Solution()
+# ss = 'aac'
+# print([i for i in s.Permutation(ss)])
+#
+# tem = "abc"
+# print(set([i for i in s.Permutation(ss)]))
+
+
 class Solution:
-    def Permutation(self, ss):
-        li = list(ss)
-        if len(li) < 1:
-            yield ss
+    # 借助递归生成器生成全排列列表组合
+    def iter_list(self, str_to_list):
+        if len(str_to_list) == 0:
+            yield str_to_list
         else:
-            for i in range(len(li)):
-                li[0], li[i] = li[i], li[0]
-                for j in self.Permutation(li[1:]):
-                    yield str(li[0]) + str(j)
+            for i in range(len(str_to_list)):
+                str_to_list[0], str_to_list[i] = str_to_list[i], str_to_list[0]
+                for j in self.iter_list(str_to_list[1:]):
+                    yield [str_to_list[0]] + j
+
+    def Permutation(self, ss):
+        if len(ss) <= 1:
+            return ss
+        _to_list = list(ss)
+        result_list = []
+        for i in self.iter_list(_to_list):
+            word = ''.join(i)
+            if word not in result_list:
+                result_list.append(word)
+        return sorted(result_list)
+
 
 s = Solution()
-ss = 'aac'
-print([i for i in s.Permutation(ss)])
+ss = 'abc'
+print(s.Permutation(ss))
+
