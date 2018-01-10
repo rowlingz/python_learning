abc1149dba81366f3ccda0829a104b1e76ba13fe
rolingz <ning_ML@126.com>
2018-01-10 21:34:26 +0800

    借助 生成器进行全排列

--- jianzhi_offer/Permutation.py
+++ jianzhi_offer/Permutation.py
@@ -41,8 +41,27 @@ class Solution:
             word = ''.join(i)
             if word not in result_list:
                 result_list.append(word)
-        return sorted(result_list)
+        return self.sorted_list(result_list)
 
+    def sorted_word(self, a, b):
+        n = len(a)
+        for i in range(n):
+            asc_a, asc_b = ord(a[i]), ord(b[i])
+            if asc_a == asc_b:
+                continue
+            elif asc_a > asc_b:
+                return True
+            else:
+                return False
+
+    def sorted_list(self, str_list):
+        n = len(str_list)
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if self.sorted_word(str_list[i], str_list[j]):
+                    str_list[i], str_list[j] = str_list[j], str_list[i]
+
+        return str_list
 
 s = Solution()
 ss = 'abc'
