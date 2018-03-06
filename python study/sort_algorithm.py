3250d3700c8cb19374d652df9228f286df2bbc4c
rolingz <ning_ML@126.com>
2018-03-06 15:39:44 +0800

    sort

--- python study/sort_algorithm.py
+++ python study/sort_algorithm.py
@@ -20,6 +20,29 @@ def insert_sort(array):
     return array
 
 
+def shell_sort(array):
+    """shell排序， 将序列分成若干子序列分别进行直接插入排序"""
+    count = len(array)
+    sep = 2
+    group = count // sep
+    while group > 0:
+        # i 记录分组数
+        for i in range(0, group):
+            j = i + group
+            # j 记录各分组的索引
+            while j < count:
+                key = array[j]
+                # 进行直接插入排序
+                for k in range(j - group, -1, - group):
+                    if array[k] > key:
+                        array[k + group], array[k] = array[k], key
+                j += group
+        print(array)
+        group //= sep
+
+    return array
+
+
 def bubble_sort(array):
     """冒泡排序"""
     count = len(array)
@@ -33,7 +56,7 @@ def bubble_sort(array):
 
 
 def select_sort(array):
-    """直接选择排序"""
+    """直接选择排序，类似冒泡，减少交换次数"""
     count = len(array)
     for i in range(count):
         min_index = i
@@ -47,11 +70,74 @@ def select_sort(array):
     return array
 
 
+def quick_sort(array, left, right):
+    """快速排序， 来源冒泡"""
+    # 设定左右两边指针，初始left=0, right=n-1,
+    if left >= right:
+        return array
+    key = array[left]
+    # 保留边界值
+    start, end = left, right
+    while left < right:
+        # 右指针，比key大，right-1， 比key小, 将right左移
+        while left < right and array[right] >= key:
+            right -= 1
+        array[left] = array[right]
+        # 右指针，比key小，left+1， 比key大，将left右移
+        while left < right and array[left] <= key:
+            left += 1
+        array[right] = array[left]
+    array[right] = key
+    print(array)
+    """
+        while left < right:
+        # 右指针，比key大，right-1， 比key小，替换left, right
+        while left < right and array[right] >= key:
+            right -= 1
+        array[left], array[right] = array[left], array[right]
+        # 右指针，比key小，left+1， 比key大，替换left, right
+        while left < right and array[left] <= key:
+            left += 1
+        array[right], array[left] = array[right], array[left]
+    """
+    # 对排序后的左右分别进行快速排序
+    quick_sort(array, start, left - 1)
+    quick_sort(array, right + 1, end)
+    return array
 
+
+def merge_sort(array):
+    """归并排序， 递归划分子问题"""
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    """合并两个有序序列"""
+    i, j = 0, 0
+    result = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    print(result)
+    return result
 
 
 if __name__ == '__main__':
     array = [5, 3, 8, 6, 4]
     # insert_sort(array)
     # bubble_sort(array)
-    select_sort(array)
+    # select_sort(array)
+    # print(quick_sort(array, 0, len(array) - 1))
+    # shell_sort(array)
+    merge_sort(array)
