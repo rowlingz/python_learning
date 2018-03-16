8376f06e29c67ec150a21edf765e75964955e967
rolingz <ning_ML@126.com>
2018-03-16 16:33:19 +0800

    pandas

--- DataAnalysis/pandas_.py
+++ DataAnalysis/pandas_.py
@@ -76,16 +76,16 @@ del frame['debt']
 
 
 # reindex 重新索引
-obj = pd.Series([1, 2, 3], index=['a', 'b', 'c'])
-obj2 = obj.reindex(['a', 'nn', 'c', 'ff'], fill_value=0)
+obj = pd.Series([5, 9, 3], index=['a', 'b', 'c'])
+obj2 = obj.reindex(['a', 'nn', 'c', 'ff'])
 obj3 = obj.reindex(['a', 'nn', 'c', 'ff'], method='pad')
 
-data_frame = pd.DataFrame(np.arange(9).reshape((3, 3)), index=['a', 'b', 'c'], columns=['mary', 'kity', 'sum'])
-frame1 = data_frame.reindex(['a', 'e', 'c', 'b'], fill_value=14)
+data_frame = pd.DataFrame(np.arange(9).reshape((3, 3)), index=['a', 'c', 'b'], columns=['mary', 'kity', 'sum'])
+frame1 = data_frame.reindex(['a', 'e', 'c', 'f'], fill_value=14)
 
 frame2 = data_frame.reindex(columns=['kity', 'jack'])
 
-frame3 = data_frame.reindex(index=['a', 'e', 'c', 'b'], columns=['kity', 'jack'])
+frame3 = data_frame.reindex(index=['a', 'e', 'c', 'b'], columns=['kity', 'jack', 'mary'])
 
 
 # 删除指定轴上的项 drop 得到新列
@@ -96,14 +96,82 @@ frame_drop = data_frame.drop('a')
 # 删除指定列
 frame_drop1 = data_frame.drop('sum', axis=1)
 
+
+# 函数
+def f(x):
+    return pd.Series([x.max(), x.min()], index=['max', 'min'])
+
+
+frame_f = data_frame.apply(f, axis=1)
+
+# 对行/列索引进行排序，得到新的对象，sort_index
+frame_sort = data_frame.sort_index()        # 对行索引进行排序
+frame_sort1 = data_frame.sort_index(axis=1, ascending=False)     # 对列索引进行排列
+
+# 对数据结构中的值进行排序, sort_values
+frame_sort_value = frame3.sort_values(by='kity')
+
+# 求出值所在排名
+obj_rank = obj.rank(ascending=False, method='first')
+frame_rank = frame_f.rank(ascending=False)
+
+# 索引是否唯一，返回True / False
+index_unique = obj.index.is_unique
+index_unique1 = frame.index.is_unique
+
+# 仅出现一次的值
+value_unique = obj.unique()
+value_unique1 = frame1.iloc[:, 1].unique()
+value_unique2 = frame1.apply(lambda x: len(x.unique()))
+
+# 值计数 value_counts()
+obj_counts = (obj * 2).value_counts()
+frame_counts = frame1.iloc[:, 2].value_counts()
+frame_counts1 = frame1.apply(lambda x: x.value_counts())
+
+# isin 值是否包含在指定序列中  bool
+obj_isin = obj.isin([3, 9])
+frame_isin = frame1.isin([5, 7])
+
+# 缺失值
+frame_null = frame3.isnull()
+obj_null = obj2.isnull()
+
+# 滤掉缺失值 dropna()
+obj_filter = obj2[obj2.notnull()]
+obj_filter1 = obj2.dropna()
+
+frame_filter = frame3.dropna()          # 删除含NA的行和列
+frame_filter1 = frame3.dropna(how='all', axis=1)    # 删除全为NA的行/列
+frame_filter2 = frame3.dropna(thresh=2)     # thresh ---int value : require that many non-NA values
+
+# 填充 fillna()
+obj_fillna = obj2.fillna(obj2.mean())
+frame_fillna = frame3.fillna(method='ffill')
+frame_fillna1 = frame3.fillna({'kity': 10, 'jack': 7})
+
+# 层次化索引（多层索引）
+multiIndex_data = pd.Series(np.random.randn(10),
+                            index=[['a', 'a', 'a', 'b', 'b', 'b', 'c', 'c', 'd', 'd'],
+                                   [1, 2, 3, 1, 2, 3, 1, 2, 1, 3]])
+
+
+
+
 if __name__ == "__main__":
     # print(frame)
     # # print(frame_to_series1)
     # print(frame.values)
     # print('four' in frame.index)
-    print(obj)
-    print("+++++++++++++++++")
-    print(obj_drop)
+    # print(obj2)
+    # print("+++++++++++++++++")
+    # print(obj_fillna)
 
-    print(data_frame)
-    print(frame_drop)
+    # print(data_frame)
+    print(frame3)
+    print(frame3.ix[:, 1])
+    # print(frame_fillna1)
+
+    # print(multiIndex_data)
+    # print(multiIndex_data[:, 2])
+    # print(multiIndex_data.unstack())
